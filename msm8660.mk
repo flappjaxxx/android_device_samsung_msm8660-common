662c290e740acdf1418405b1e76b93ee1671e36e
flappjaxxx <flappjaxxx@gmail.com>
2013-02-25 18:57:14 -0500

    Revert "Temporary flicker workaround"
    This reverts commit cab9476cd8ac950b705408e4b70eb67afb807a31.

--- msm8660.mk
+++ msm8660.mk
@@ -139,7 +139,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     com.qc.hardware=true \
     ro.vendor.extension_library=/system/lib/libqc-opt.so \
     ro.hwui.text_cache_width=2048 \
-    debug.mdpcomp.maxlayer=2
+    debug.mdpcomp.maxlayer=3
 
 # Common overlay
 DEVICE_PACKAGE_OVERLAYS += device/samsung/msm8660-common/overlay
